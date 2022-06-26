b27437a1d280f20fdd5bce7ef91112a2cd6c4489
Beth Njoroge <bethnjoroge19@gmail.com>
2022-06-26 10:51:02 -0700

    printf commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+*main ->this is a function to print hello world
+*Return: success always 0
+*/
 int void main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
