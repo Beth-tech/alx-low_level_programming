06304f657bb5113bbc4a2df5fd73cf3e7866bb4c
Beth Njoroge <bethnjoroge19@gmail.com>
2022-06-26 04:27:18 -0700

    Hello,puts commit

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>                                                                                                                   
+
+                                                                                                                                      
+int main()                                                                                                                            
+
+{                                                                                                                                     
+
+	 printf("anything");                                                                                                                  
+
+	  return 0;                                                                                                                            
+}  
