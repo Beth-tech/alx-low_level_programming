8231a7200619769343ad0a6897c71bc1fa822744
Beth Njoroge <bethnjoroge19@gmail.com>
2022-06-26 10:36:41 -0700

    commit for puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+*main ->this is a function to print hello world
+*Return: success always 0
+*/
 int  main(void)
 {
 puts("programming is like building a multillingual puzzle\n");
