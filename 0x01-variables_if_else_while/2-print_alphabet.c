45c3da7fb207f23419a49184e32c2047f3fab656
Beth Njoroge <bethnjoroge19@gmail.com>
2022-07-01 06:03:11 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -> Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int ch;
+for ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+putchar(10); /*this is an ascii code for new line*/
+return (0);
+}
