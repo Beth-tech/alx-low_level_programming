3815a0dea0b90dda42cdba9395593da4b5105acf
Beth Njoroge <bethnjoroge19@gmail.com>
2022-07-14 06:41:12 -0700

    hot fix 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,45 +10,37 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, a, l, n;
-	int c = 0;
+	int i, j, k, l, m, n;
 
-	for (i = 0; n1[i] != '\0'; i++)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-	}
-	for (j = 0; n2[j] != '\0'; j++)
-	{
-	}
-	for (k = 0; k < i || k < j; k++)
-	{
-		n = 0;
-		if (i > k)
-			n = n + n1[i - k - 1] - '0';
-		if (j > k)
-			n = n + n2[j - k - 1] - '0';
-		n = n + c;
-		c = 0;
-		if (n > 9)
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
 		{
-			r[k] = ((n - 10) + '0');
-			c = 1;
+			break;
 		}
-		else
-			r[k] = n + '0';
-	}
-	if (c == 1)
-	{
-		r[k] = '1';
-		k++;
-	}
-		for (l = 0; l < k / 2; l++)
-	{
-		a = r[l];
-		r[l] = r[k - l - 1];
-		r[k - l - 1] = a;
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
 	r[k] = '\0';
-	if (k >= size_r)
+	if (i >= 0 || j >= 0 || m)
 		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
 	return (r);
 }
