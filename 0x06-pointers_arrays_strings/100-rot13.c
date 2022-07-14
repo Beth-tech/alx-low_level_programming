96ef7c800460aa47e4f254f5a10a448409a718e2
Beth Njoroge <bethnjoroge19@gmail.com>
2022-07-14 05:44:20 -0700

    hot fix 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,27 +2,27 @@
 
 /**
  * rot13 - Rot13
- * @a: Array
- * Return: Rot13
+ * @k: Array
+ * Return: array k
  */
-char *rot13(char *a)
+
+char *rot13(char *k)
 {
-	int j, i;
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	char b1[] = "abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWYZ";
-	char b2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+int i = 0;
+int j = 0;
 
-	for (i = 0; a[i] != '\0'; i++)
-	{
-		for (j = 0; b1[j] != '\0'; j++)
-		{
-			if (a[i] == b1[j])
-			{
-				a[i] = b2[j];
-				break;
-			}
-		}
-	}
-
-	return (a);
+while (k[i] != '\0')
+{
+for (j = 0; a[j] != '\0' && k[i] != a[j]; j++)
+;
+if (j < 52)
+{
+k[i] = b[j];
+}
+i++;
+}
+return (k);
 }
