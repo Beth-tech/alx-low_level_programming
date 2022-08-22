6a187192c6b561ad9270fd8c5efd21380a90d0e3
Beth-tech <107035899+Beth-tech@users.noreply.github.com>
2022-08-22 20:25:00 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,86 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
-void read_write(int file_from, int file_to, char *name_from, char *name_to);
-
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments - 3 or error
- * @argv: array with arguments
- * first name of file to be copy
- * second name of the new file
- * Return: 0
- */
+* main - program that copies the content of a file to another file
+* @argc: num argument
+* @argv: string argument
+* Return: 0
+*/
+
 int main(int argc, char *argv[])
 {
-int file_to, file_from, check;
+int file_from, file_to;
+int num1 = 1024, num2 = 0;
+char buf[1024];
 
 if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
 }
-file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH);
 if (file_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	close(file_from), exit(99);
+}
+while (num1 == 1024)
+{
+	num1 = read(file_from, buf, 1024);
+	if (num1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	num2 = write(file_to, buf, num1);
+	if (num2 < num1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
 
-read_write(file_from, file_to, argv[1], argv[2]);
+if (close(file_from) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 
-check = close(file_from);
-if (check == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
-}
-check = close(file_to);
-if (check == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-exit(100);
-}
-check = close(file_to);
-if (check == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-exit(100);
-}
+if (close(file_to) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+
 return (0);
-}
-
-/**
- * read_write - aux function. Reads the From file and write in the To file
- * @file_from: File descriptor for From file
- * @file_to: File descriptor for To file
- * @name_from: String with name of From file
- * @name_to: String with name of To file
- */
-void read_write(int file_from, int file_to, char *name_from, char *name_to)
-{
-int num = 1024, check;
-char buffer[1024];
-
-while (num == 1024)
-{
-num = read(file_from, buffer, 1024);
-if (num == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name_from);
-exit(98);
-}
-
-check = write(file_to, buffer, num);
-if (check == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name_to);
-exit(99);
-}
-}
 }
